64da461966d46e8b435546e5e8c6192f7dc1308f
Anthony Nguyen <an042@hotmail.de>
2017-04-24 16:25:25 +0200

    .test.

--- src/de/hdm/itprojekt/server/test.java
+++ src/de/hdm/itprojekt/server/test.java
@@ -8,4 +8,22 @@ public class test {
 	int a = 21;
 	
 	int b = 22;
+	
+	int c = 23;
+
+	int d = 11;
+	
+ int test = 123456; 
+ int e = 12;
+int f = 13;
+ String test1 = "Hallo Welt"; 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+ String testaberhallo = "Mein Versuch";
 }
