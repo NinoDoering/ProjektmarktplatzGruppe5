48c21f111d4267c92cdc788c0e1b2b09249f32f6
ClirimShahini <cs223@hdm-stuttgart.de>
2017-04-27 11:18:47 +0200

    .Nachtrag-test.

--- src/de/hdm/itprojekt/client/GreetingServiceAsync.java
+++ src/de/hdm/itprojekt/client/GreetingServiceAsync.java
@@ -8,5 +8,5 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 public interface GreetingServiceAsync {
 	void greetServer(String input, AsyncCallback<String> callback) throws IllegalArgumentException;
 	
-	//wichtig!
+	//wichtig!!
 }
