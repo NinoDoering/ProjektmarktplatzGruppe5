4dffd57a702dc86f10056d6be866665ccdb086e4
ThomasBurkhardt <tb112@hdm-stuttgart.de>
2017-05-09 12:34:51 +0200

    Aktualisierung PartnerProfilMapper + Formatierung aller Mapper

--- src/de/hdm/itprojekt/server/db/PersonMapper.java
+++ src/de/hdm/itprojekt/server/db/PersonMapper.java
@@ -8,169 +8,155 @@ import de.hdm.itprojekt.shared.bo.*;
 public class PersonMapper {
 
 	private static PersonMapper personMapper = null;
-	
+
 	protected PersonMapper() {
-	  }
-	
+	}
+
 	public static PersonMapper personMapper() {
-	    if (personMapper == null) {
-	      personMapper = new PersonMapper();
-	    }
+		if (personMapper == null) {
+			personMapper = new PersonMapper();
+		}
 
-	    return personMapper;
-	  }
-	
-	//FindByKey
+		return personMapper;
+	}
+
+	// FindByKey
 	public Person findByKey(int id) {
-	    Connection con = DBConnection.connection();
+		Connection con = DBConnection.connection();
 
-	    try {
-	      Statement stmt = con.createStatement();
+		try {
+			Statement stmt = con.createStatement();
 
-	      ResultSet rs = stmt
-	          .executeQuery("SELECT id, vorname, nachname, geschlecht FROM person "
-	              + "WHERE id=" + id + " ORDER BY nachname");
+			ResultSet rs = stmt.executeQuery(
+					"SELECT id, vorname, nachname, geschlecht FROM person " + "WHERE id=" + id + " ORDER BY nachname");
 
-	      
-	      if (rs.next()) {
-	        Person p = new Person();
-	        p.setId(rs.getInt("id"));
-	        p.setVorname(rs.getString("vorname"));
-	        p.setNachname(rs.getString("nachname"));
-	        p.setGeschlecht(rs.getChar("geschlecht")); //Was tun hier? 
-	        										   //char undefined for rs.
+			if (rs.next()) {
+				Person p = new Person();
+				p.setId(rs.getInt("id"));
+				p.setVorname(rs.getString("vorname"));
+				p.setNachname(rs.getString("nachname"));
+				p.setGeschlecht(rs.getChar("geschlecht")); // Was tun hier?
+															// char undefined
+															// for rs.
 
-	        return p;
-	      }
-	    }
-	    catch (SQLException e) {
-	      e.printStackTrace();
-	      return null;
-	    }
+				return p;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
 
-	    return null;
-	  }
-	
-	//FindAll
-	 public Vector<Person> findAll() {
-		    Connection con = DBConnection.connection();
-		    Vector<Person> result = new Vector<Person>();
+		return null;
+	}
 
-		    try {
-		      Statement stmt = con.createStatement();
+	// FindAll
+	public Vector<Person> findAll() {
+		Connection con = DBConnection.connection();
+		Vector<Person> result = new Vector<Person>();
 
-		      ResultSet rs = stmt.executeQuery("SELECT id, vorname, nachname "
-		          + "FROM person " + "ORDER BY nachname");
+		try {
+			Statement stmt = con.createStatement();
 
-		      while (rs.next()) {
-		        Person p = new Person();
-		        p.setId(rs.getInt("id"));
-		        p.setVorname(rs.getString("vorname"));
-		        p.setNachname(rs.getString("nachname"));
+			ResultSet rs = stmt.executeQuery("SELECT id, vorname, nachname " + "FROM person " + "ORDER BY nachname");
 
-		        result.addElement(p);
-		      }
-		    }
-		    catch (SQLException e2) {
-		      e2.printStackTrace();
-		    }
+			while (rs.next()) {
+				Person p = new Person();
+				p.setId(rs.getInt("id"));
+				p.setVorname(rs.getString("vorname"));
+				p.setNachname(rs.getString("nachname"));
 
-		    return result;
-		  }
-	 
-	 public Vector<Person> findByNachname(String nachname) {
-		    Connection con = DBConnection.connection();
-		    Vector<Person> result = new Vector<Person>();
+				result.addElement(p);
+			}
+		} catch (SQLException e2) {
+			e2.printStackTrace();
+		}
 
-		    try {
-		      Statement stmt = con.createStatement();
+		return result;
+	}
 
-		      ResultSet rs = stmt.executeQuery("SELECT id, vorname, nachname "
-		          + "FROM person " + "WHERE nachname LIKE '" + nachname
-		          + "' ORDER BY nachname");
+	public Vector<Person> findByNachname(String nachname) {
+		Connection con = DBConnection.connection();
+		Vector<Person> result = new Vector<Person>();
 
-		      
-		      while (rs.next()) {
-		        Person p = new Person();
-		        p.setId(rs.getInt("id"));
-		        p.setVorname(rs.getString("vorname"));
-		        p.setNachname(rs.getString("nachname"));
+		try {
+			Statement stmt = con.createStatement();
 
-		        result.addElement(p);
-		      }
-		    }
-		    catch (SQLException e3) {
-		      e3.printStackTrace();
-		    }
+			ResultSet rs = stmt.executeQuery("SELECT id, vorname, nachname " + "FROM person " + "WHERE nachname LIKE '"
+					+ nachname + "' ORDER BY nachname");
 
-		    return result;
-		  }
-	
-	 
-	 //INSERT
-	 public Person insert(Person p) {
-		    Connection con = DBConnection.connection();
+			while (rs.next()) {
+				Person p = new Person();
+				p.setId(rs.getInt("id"));
+				p.setVorname(rs.getString("vorname"));
+				p.setNachname(rs.getString("nachname"));
 
-		    try {
-		      Statement stmt = con.createStatement();
+				result.addElement(p);
+			}
+		} catch (SQLException e3) {
+			e3.printStackTrace();
+		}
 
-		      ResultSet rs = stmt.executeQuery("SELECT MAX(id) AS maxid "
-		          + "FROM person ");
+		return result;
+	}
 
-		      if (rs.next()) {
+	// INSERT
+	public Person insert(Person p) {
+		Connection con = DBConnection.connection();
 
-		        p.setId(rs.getInt("maxid") + 1);
+		try {
+			Statement stmt = con.createStatement();
 
-		        stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT MAX(id) AS maxid " + "FROM person ");
 
-		        stmt.executeUpdate("INSERT INTO person (id, vorname, nachname) "
-		            + "VALUES (" + p.getId() + ",'" + p.getVorname() + "','"
-		            + p.getNachname() + "')");
-		      }
-		    }
-		    catch (SQLException e4) {
-		      e4.printStackTrace();
-		    }
+			if (rs.next()) {
 
-		    return p;
-		  }
-	 
-	 //UPDATE
-	 public Person update(Person p) {
-		    Connection con = DBConnection.connection();
+				p.setId(rs.getInt("maxid") + 1);
 
-		    try {
-		      Statement stmt = con.createStatement();
+				stmt = con.createStatement();
 
-		      stmt.executeUpdate("UPDATE person " + "SET vorname=\""
-		          + p.getVorname() + "\", " + "nachname=\"" + p.getNachname() + "\" "
-		          + "WHERE id=" + p.getId());
+				stmt.executeUpdate("INSERT INTO person (id, vorname, nachname) " + "VALUES (" + p.getId() + ",'"
+						+ p.getVorname() + "','" + p.getNachname() + "')");
+			}
+		} catch (SQLException e4) {
+			e4.printStackTrace();
+		}
 
-		    }
-		    catch (SQLException e5) {
-		      e5.printStackTrace();
-		    }
+		return p;
+	}
 
-		    return p;
-		  }
-	 
-	 //DELETE
-	 public void delete(Person p) {
-		    Connection con = DBConnection.connection();
+	// UPDATE
+	public Person update(Person p) {
+		Connection con = DBConnection.connection();
 
-		    try {
-		      Statement stmt = con.createStatement();
+		try {
+			Statement stmt = con.createStatement();
 
-		      stmt.executeUpdate("DELETE FROM person " + "WHERE id=" + p.getId());
-		    }
-		    catch (SQLException e6) {
-		      e6.printStackTrace();
-		    }
-		  }
-	 
-	 //FAN-IN-FAN-OUT-Analyse -->RICHTIG??
-	 public Vector<Person> getPersonOf(Person p) {
+			stmt.executeUpdate("UPDATE person " + "SET vorname=\"" + p.getVorname() + "\", " + "nachname=\""
+					+ p.getNachname() + "\" " + "WHERE id=" + p.getId());
 
-		    return PersonMapper.personMapper().findByKey(p); //FALSCH, WARUM?
-		  }
+		} catch (SQLException e5) {
+			e5.printStackTrace();
+		}
+
+		return p;
+	}
+
+	// DELETE
+	public void delete(Person p) {
+		Connection con = DBConnection.connection();
+
+		try {
+			Statement stmt = con.createStatement();
+
+			stmt.executeUpdate("DELETE FROM person " + "WHERE id=" + p.getId());
+		} catch (SQLException e6) {
+			e6.printStackTrace();
+		}
+	}
+
+	// FAN-IN-FAN-OUT-Analyse -->RICHTIG??
+	public Vector<Person> getPersonOf(Person p) {
+
+		return PersonMapper.personMapper().findByKey(p); // FALSCH, WARUM?
+	}
 }
