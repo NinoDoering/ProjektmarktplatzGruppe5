97187937816a34691448b027e3d8e1b06b8119f9
Anthony Nguyen <an042@hdm-stuttgart.de>
2017-05-12 14:14:22 +0200

    GUI neuerTest

--- src/de/hdm/itprojekt/client/Projektmarktplatz.java
+++ src/de/hdm/itprojekt/client/Projektmarktplatz.java
@@ -3,6 +3,7 @@ package de.hdm.itprojekt.client;
 import de.hdm.itprojekt.shared.FieldVerifier;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyCodes;
@@ -10,11 +11,15 @@ import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DecoratorPanel;
 import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.DockLayoutPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
@@ -27,21 +32,38 @@ public class Projektmarktplatz implements EntryPoint {
 	
 	
 	public void onModuleLoad() {
-
-		HorizontalPanel hPanel = new HorizontalPanel();
 		
+		/*    
+		HorizontalPanel h1Panel = new HorizontalPanel();
+		HorizontalPanel h2Panel = new HorizontalPanel();
 		VerticalPanel vPanel = new VerticalPanel();
-		Label myLbl = new Label("Projektmarktplatz");
 		
+
+		Label myLbl = new Label("Projektmarktplatz");
+
 		vPanel.add(myLbl);
 			
-		Button btn1 = new Button("Projektmarktplatz suchen"); 
-		hPanel.add(btn1);
+		
 		Button btn2 = new Button ("Projektmarktplatz erstellen");
-		hPanel.add(btn2);
+		h2Panel.add(btn2);
+		
+		RootPanel.get("btn").add(h1Panel);
+		RootPanel.get("btn").add(h2Panel);
+		
+		
+		
+		*/
+		
+		Button btn1 = new Button("Projektmarktplatz suchen"); 
+		
+		SimplePanel simplePanel = new SimplePanel();
+		simplePanel.add(btn1);
+		RootPanel.get("PPlatzSuch").add(simplePanel);
+
+		
+
 			
-		RootPanel.get().add(hPanel);
-		RootPanel.get().add(vPanel);
+	
 		
 	}
 	// erneut wichtig!!!
