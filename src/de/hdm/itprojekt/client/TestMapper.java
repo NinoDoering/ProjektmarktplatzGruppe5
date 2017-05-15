567d6752d7c414b567e0b077d0f1d63dc244fe9d
nino <nino@nino-PC>
2017-05-16 01:23:25 +0200

    UFF Insert, Delete, Select läuft endlich gescheit

--- src/de/hdm/itprojekt/client/TestMapper.java
+++ src/de/hdm/itprojekt/client/TestMapper.java
@@ -1,6 +1,11 @@
 package de.hdm.itprojekt.client;
 
 import de.hdm.itprojekt.server.db.PersonMapper;
+import de.hdm.itprojekt.shared.bo.Person;
+
+import java.*;		//Pakete, welche zum Ausführen benötigt werden.
+import javax.*;
+import java.sql.*;
 
 public class TestMapper {
 
@@ -9,7 +14,42 @@ public class TestMapper {
 
 	//	PersonMapper.personMapper().findPersonByKey(1);
 		
-		System.out.println(PersonMapper.personMapper().findPersonByKey(1));
+		Person eins = PersonMapper.personMapper().findPersonByKey(2);
+		
+		System.out.println(PersonMapper.personMapper().findPersonByKey(2));
+		
+		
+		System.out.println(eins.getTitel());
+		System.out.println(eins.getVorname());
+		System.out.println(eins.getNachname());
+		
+	
+		
+		Person p = new Person(); 
+		
+		p.setIdPerson(3);
+		p.setTitel("Dr");
+		p.setVorname("Johnny");
+		p.setNachname("Depp");
+		
+		 
+		// PersonMapper.personMapper().insert(p); 
+		
+		
+		
+		
+		//PersonMapper.personMapper().delete(3);
+		
+		
+		
+		
+		
+		Person p2 = new Person(); 
+		PersonMapper.personMapper().update(p2); 
+	
+	p2.setIdBusinessObject(3); 
+	p2.setNachname("Trottel");
+	
 		
 		
 		
