9f722f7886fe13055c8285d89f5b867b8af2c2c2
ThomasBurkhardt <tb112@hdm-stuttgart.de>
2017-05-16 15:35:30 +0200

    .ProjektmarktplatzAdministrationImpl - Organisationseinheit, Partnerprofil

--- src/de/hdm/itprojekt/server/ProjektmarktplatzAdministrationImpl.java
+++ src/de/hdm/itprojekt/server/ProjektmarktplatzAdministrationImpl.java
@@ -12,26 +12,27 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 public class ProjektmarktplatzAdministrationImpl extends RemoteServiceServlet
 		implements ProjektmarktplatzAdministration {
 
-
-	//private BusinessObjectMapper boMapper = null;
+	// private BusinessObjectMapper boMapper = null;
 	private EigenschaftMapper eigMapper = null;
-	private OrganisationseinheitMapper orgaMapper = null; // Bleibt die Mapper bestehen?
+	private OrganisationseinheitMapper orgaMapper = null; // Bleibt die Mapper
+															// bestehen?
 	private PartnerprofilMapper ppMapper = null;
 	private PersonMapper persMapper = null;
 	private ProjektMapper prjktMapper = null;
 	private ProjektmarktplatzMapper pmpMapper = null;
 	private TeamMapper teamMapper = null;
 	private UnternehmenMapper unternehmenMapper = null;
-	
-	//No-Argument-Konstruktor
-	public ProjektmarktplatzAdministrationImpl() throws IllegalArgumentException{
-		
+
+	// No-Argument-Konstruktor
+	public ProjektmarktplatzAdministrationImpl() throws IllegalArgumentException {
+
 	}
 
-	//Initialisierung
-	public void init() throws IllegalArgumentException{
-		
-		//this.boMapper = BusinessObjectMapper.businessObjectMapper(); (Klasse wird gelöscht)
+	// Initialisierung
+	public void init() throws IllegalArgumentException {
+
+		// this.boMapper = BusinessObjectMapper.businessObjectMapper(); (Klasse
+		// wird gelöscht)
 		this.eigMapper = EigenschaftMapper.eigenschaftMapper();
 		this.orgaMapper = OrganisationseinheitMapper.organisationseinheitMapper();
 		this.ppMapper = PartnerprofilMapper.partnerprofilMapper();
@@ -39,51 +40,100 @@ public class ProjektmarktplatzAdministrationImpl extends RemoteServiceServlet
 		this.prjktMapper = ProjektMapper.projektMapper();
 		this.pmpMapper = ProjektmarktplatzMapper.projektmarktplatzMapper();
 		this.teamMapper = TeamMapper.teamMapper();
-		this.unternehmenMapper = UnternehmenMapper.unternehmenMapper();	
+		this.unternehmenMapper = UnternehmenMapper.unternehmenMapper();
+	}
+
+	// createEigenschaft
+	public Eigenschaft createEigenschaft(
+			/* int idEigenschaft, */
+			String arbeitsgebiet, String ausbildung, float berufserfahrungsJahre, String sprachkenntnisse,
+			String employmentStatus, String abschluss) throws IllegalArgumentException {
+		Eigenschaft eig = new Eigenschaft();
+
+		/* eig.setIdEigenschaft(idEigenschaft); Ist diese Methode richtig? */
+		eig.setArbeitsgebiet(arbeitsgebiet);
+		eig.setAusbildung(ausbildung);
+		eig.setBerufserfahrungsJahre(berufserfahrungsJahre);
+		eig.setSprachkenntnisse(sprachkenntnisse);
+		eig.setEmploymentStatus(employmentStatus);
+		eig.setAbschluss(abschluss);
+
+		return this.eigMapper.insert(eig);
+	}
+
+	// getEigenschaftById
+	public Eigenschaft getEigenschaftById(int idEigenschaft) throws IllegalArgumentException {
+		return this.eigMapper.findByKey(idEigenschaft);
+	}
+
+	// getAll
+	public Vector<Eigenschaft> getAllEigenschaften() throws IllegalArgumentException {
+		return this.eigMapper.findAll();
 	}
 	
-	//createEigenschaft
-		public Eigenschaft createEigenschaft(
-		/*int idEigenschaft,*/ 
-		String arbeitsgebiet,
-		String ausbildung,
-		float berufserfahrungsJahre,
-		String sprachkenntnisse,
-		String employmentStatus,
-		String abschluss
-		) throws IllegalArgumentException {
-			Eigenschaft eig = new Eigenschaft();
-			
-			/*eig.setIdEigenschaft(idEigenschaft);		Ist diese Methode richtig?*/
-			eig.setArbeitsgebiet(arbeitsgebiet);
-			eig.setAusbildung(ausbildung);
-			eig.setBerufserfahrungsJahre(berufserfahrungsJahre);
-			eig.setSprachkenntnisse(sprachkenntnisse);
-			eig.setEmploymentStatus(employmentStatus);
-			eig.setAbschluss(abschluss);
-			
-			return this.eigMapper.insert(eig);
-		}
+	// updateEigenschaft
+	public void updateEigenschaft(Eigenschaft eig) throws IllegalArgumentException {
+		eigMapper.update(eig);		
+	}
+	
+	// deleteEigenschaft
+	public void deleteEigenschaft(Eigenschaft eig) throws IllegalArgumentException {
+		this.eigMapper.delete(eig);
+	}
+	
+	
+// START ORGANISATIONSEINHEIT____________________________________________
+	
+	
+	// createOrganisationseinheit
+	public Organisationseinheit createOrganisationseinheit(int idOrganisationseinheit) throws IllegalArgumentException {
+		Organisationseinheit orgaEinheit = new Organisationseinheit();
+		orgaEinheit.setIdOrganisationseinheit(idOrganisationseinheit);
+
+		return this.orgaMapper.insert(orgaEinheit);
+	}
+
+	// getOrganisationseinheitById
+	public Organisationseinheit getOrganisationseinheitById(int idOrganisationseinheit) throws IllegalArgumentException {
+		return this.orgaMapper.findByKey(idOrganisationseinheit);
+	}
+	
+	// updateOrganisationseinheit    #############################################
+	//############################################################################
+	
+	
+	// deleteOrganisationseinheit
+	public void deleteOrganisationseinheit(Organisationseinheit orgaEinheit) throws IllegalArgumentException {
+		this.orgaMapper.delete(orgaEinheit);
+	}
+	
+	
+	
+	/*##########################################################
+	 * START PARTNERPROFIL
+	 #########################################################*/
+	
+	// createPartnerprofil
+	public Partnerprofil createPartnerprofil(int idPartnerprofil) throws IllegalArgumentException {
+		Partnerprofil pp = new Partnerprofil();
+		pp.setIdPartnerprofil(idPartnerprofil);
 		
-		//getEigenschaftById
-		public Eigenschaft getEigenschaftById(int idEigenschaft) 
-				throws IllegalArgumentException{
-			return this.eigMapper.findByKey(idEigenschaft);
-		}
-		
-		//getAll
-		public Vector <Eigenschaft> getAll()
-				throws IllegalArgumentException{
-			return this.eigMapper.findAll();
-		}
-		
-		//createEigenschaft
-		public Organisationseinheit createEigenschaft(int idOrganisationseinheit)
-				throws IllegalArgumentException {
-			Organisationseinheit orgaEinheit = new Organisationseinheit();
-			orgaEinheit.setIdOrganisationseinheit(idOrganisationseinheit);
-			
-			return this.orgaMapper.insert(orgaEinheit);
-		}
+		return this.ppMapper.insert(pp);
+	}
+	
+	// getPartnerprofilById
+	public Partnerprofil getPartnerprofilById(int idPartnerprofil){
+		return this.ppMapper.findByKey(idPartnerprofil);
+	}
+	
+	// getAllPartnerprofile
+	public Vector <Partnerprofil> getAllPartnerprofile() throws IllegalArgumentException {
+		return this.ppMapper.findAll();
+	}
+	
+	// updatePartnerprofil
+	public void updatePartnerprofil(Partnerprofil pp) throws IllegalArgumentException {
+		ppMapper.update(pp);		
+	}
 	
 }
