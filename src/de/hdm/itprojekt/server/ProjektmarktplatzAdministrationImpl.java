3f7d1aa924cb6be10fce820401f630772f84dced
ThomasBurkhardt <tb112@hdm-stuttgart.de>
2017-05-19 17:18:05 +0200

    .ProjektmarktplatzAdministrationImpl - Unternehmen, Marktplatz, Team + hinzufügen von Variablen

--- src/de/hdm/itprojekt/server/ProjektmarktplatzAdministrationImpl.java
+++ src/de/hdm/itprojekt/server/ProjektmarktplatzAdministrationImpl.java
@@ -1,6 +1,7 @@
 package de.hdm.itprojekt.server;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Vector;
 
 import de.hdm.itprojekt.server.db.*;
@@ -19,13 +20,16 @@ public class ProjektmarktplatzAdministrationImpl extends RemoteServiceServlet
 	// private BusinessObjectMapper boMapper = null;
 	private EigenschaftMapper eigMapper = null;
 	private OrganisationseinheitMapper orgaMapper = null; // Bleibt die Mapper
-															// bestehen?
+	private MarktplatzMapper mpMapper = null;														// bestehen?
 	private PartnerprofilMapper ppMapper = null;
 	private PersonMapper persMapper = null;
 	private ProjektMapper prjktMapper = null;
 	private ProjektmarktplatzMapper pmpMapper = null;
 	private TeamMapper teamMapper = null;
 	private UnternehmenMapper unternehmenMapper = null;
+	private AusschreibungMapper ausschreibungMapper = null;
+	private BeteiligungMapper beteiligungMapper = null;
+	private BewerbungMapper bewerbungMapper = null;
 
 	// No-Argument-Konstruktor
 	public ProjektmarktplatzAdministrationImpl() throws IllegalArgumentException {
@@ -39,13 +43,17 @@ public class ProjektmarktplatzAdministrationImpl extends RemoteServiceServlet
 		// wird gelöscht)
 		this.eigMapper = EigenschaftMapper.eigenschaftMapper();
 		this.orgaMapper = OrganisationseinheitMapper.organisationseinheitMapper();
+		this.mpMapper = MarktplatzMapper.marktplatzMapper();
 		this.ppMapper = PartnerprofilMapper.partnerprofilMapper();
 		this.persMapper = PersonMapper.personMapper();
 		this.prjktMapper = ProjektMapper.projektMapper();
 		this.pmpMapper = ProjektmarktplatzMapper.projektmarktplatzMapper();
 		this.teamMapper = TeamMapper.teamMapper();
 		this.unternehmenMapper = UnternehmenMapper.unternehmenMapper();
-	}
+		this.ausschreibungMapper = AusschreibungMapper.ausschreibungmapper(); 
+											//.ausschreibungMapper muss das heißen auch in Mapperklasse
+		this.bewerbungMapper = BewerbungMapper.bewerbungmapper(); //gleicher Fehler!!!!!
+		}
 
 	// createEigenschaft
 	public Eigenschaft createEigenschaft(
@@ -194,5 +202,182 @@ public class ProjektmarktplatzAdministrationImpl extends RemoteServiceServlet
 		this.persMapper.delete(idPerson);
 		}
 	
-	// 
+	/*##########################################################
+	 * START PROJEKT
+	 #########################################################*/
+	
+	// createProjekt
+	public Projekt createProjekt(
+			int idProjekt,
+			String bezeichnung,
+			String beschreibung,
+			Date startDatum,
+			Date endDatum,
+			Person projektleiter
+			) throws IllegalArgumentException {
+		Projekt projekt = new Projekt();
+		
+		projekt.setIdProjekt(idProjekt);
+		projekt.setBezeichnung(bezeichnung);
+		projekt.setBeschreibung(beschreibung);
+		projekt.setStartDatum(startDatum);
+		projekt.setEndDatum(endDatum);
+		projekt.setProjektleiter(projektleiter);
+		
+		return this.prjktMapper.insert(projekt);
+	}
+	
+	// getProjektById
+	public Projekt getProjektById(int idProjekt) throws IllegalArgumentException {
+		return this.prjktMapper.findByKey(idProjekt);
+	}
+	
+	// getAllProjekte
+	public Vector<Projekt> getAllProjekte() throws IllegalArgumentException {
+		return this.prjktMapper.findAll();
+	}
+	
+	// getProjektByBezeichnung
+	public Vector<Projekt> getProjektByBezeichnung(String bezeichnung) throws IllegalArgumentException {
+		return this.prjktMapper.findByBezeichnung(bezeichnung);
+	}
+	
+	// updateProjekt
+	public void updateProjekt(Projekt projekt) throws IllegalArgumentException {
+		prjktMapper.update(projekt);
+	}
+	
+	//deleteProjekt
+	public void deleteProjekt(int idProjekt) throws IllegalArgumentException {
+		this.prjktMapper.delete(idProjekt);
+		}
+	
+	/*##########################################################
+	 * START MARKTPLATZ
+	 #########################################################*/
+	
+	//createMarktplatz
+	public void createMarktplatz(
+			int idProjektmarktplatz,
+			String geschaeftsgebiet,
+			String bezeichnung
+			) throws IllegalArgumentException{
+		Marktplatz marktplatz = new Marktplatz();
+		
+		marktplatz.setIdMarktplatz(idMarktplatz);
+		marktplatz.setGeschaeftsgebiet(geschaeftsgebiet);
+		marktplatz.setBezeichnung(bezeichnung);
+		
+		return this.pmpMapper.insert(marktplatz);
+	}
+	
+	// getMarktplatzById
+	public Marktplatz findByKey(int idMarktplatz) throws IllegalArgumentException {
+		return this.mpMapper.findByKey(idMarktplatz);
+	}
+	
+	// getAllMarktplaetze
+	public Vector<Marktplatz> getAllMarktplaetze() throws IllegalArgumentException {
+		return this.mpMapper.findAll();
+	}
+	
+	// getMarktplatzByBezeichnung
+	public Vector<Marktplatz> getMarktplatzByBezeichnung(String bezeichnung) throws IllegalArgumentException {
+		return this.mpMapper.findByBezeichnung(bezeichnung);
+	}
+	
+	// updateMarktplatz
+	public void updateMarktplatz(Marktplatz marktplatz) throws IllegalArgumentException {
+		mpMapper.update(marktplatz);
+	}
+	
+	// deleteMarktplatz
+	public void deleteMarktplatz(int idMarktplatz) throws IllegalArgumentException {
+		this.mpMapper.delete(idMarktplatz);
+		}
+	
+	/*##########################################################
+	 * START TEAM
+	 #########################################################*/
+	
+	// createTeam
+	public void createTeam(
+			int idTeam,
+			String teamName,
+			int mitgliederAnzahl
+			) throws IllegalArgumentException {
+		
+		Team team = new Team();
+		
+		team.setId(idTeam);
+		team.setTeamName(teamName);
+		team.setMitgliederAnzahl(mitgliederAnzahl);
+		
+		return this.teamMapper.insert(team);
+	}
+	
+	// getTeamById
+	public Team getTeamById(int idTeam) throws IllegalArgumentException {
+		return this.teamMapper.findbyKey(idTeam);
+	}
+	
+	// getAllTeams
+	public Vector<Team> getAllTeams() throws IllegalArgumentException {
+		return this.teamMapper.findAll();
+	}
+	
+	// getTeamByTeamName
+	public Vector<Team> getTeamByTeamName(String teamName) throws IllegalArgumentException {
+		return this.teamMapper.findByTeamName(teamName);
+	}
+	
+	// updateTeam
+		public void updateTeam(Team team) throws IllegalArgumentException {
+			teamMapper.update(team);
+		}
+		
+	// deleteTeam
+		public void deleteTeam(int idTeam) throws IllegalArgumentException {
+			this.teamMapper.delete(idTeam);
+		}
+		
+		/*##########################################################
+		 * START UNTERNEHMEN
+		 #########################################################*/	
+
+		// createUnternehmen
+		public void createUnternehmen(
+				String firmenName,
+				int idUnternehmen
+				) throws IllegalArgumentException {
+	
+			Unternehmen unternehmen = new Unternehmen();
+			
+			unternehmen.setIdUnternehmen(idUnternehmen);
+			unternehmen.setFirmenName(firmenName);
+			
+			return this.unternehmenMapper.insert(unternehmen);
+		}
+		
+		// getUnternehmenByFirmenName
+		public Unternehmen getUnternehmenByFirmenName(String firmenName) 
+				throws IllegalArgumentException {
+			
+			return this.getUnternehmenByFirmenName(firmenName);
+		}
+		
+		// getUnternehmenById? ############################################
+		
+		
+		// updateUnternehmen
+		public void updateUnternehmen(Unternehmen unternehmen) throws IllegalArgumentException {
+			teamMapper.update(unternehmen);
+		}
+		
+		/* deleteUnternehmen ----- 
+			Hier muss doch auch noch dann die idUnternehmen Variable erstellt werden!!
+			*/
+		public void deleteUnternehmen(int idUnternehmen) throws IllegalArgumentException {
+			this.unternehmenMapper.delete(idUnternehmen);
+		}
 }
