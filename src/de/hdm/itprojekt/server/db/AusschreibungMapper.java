087e4d5933f26dbb7b6eefe2b79f3bfe12b1d876
WahidVanaki <vw019@hdm-stuttgart.de>
2017-05-25 13:59:10 +0200

    .Syntax fehler behoben.

--- src/de/hdm/itprojekt/server/db/AusschreibungMapper.java
+++ src/de/hdm/itprojekt/server/db/AusschreibungMapper.java
@@ -32,7 +32,7 @@ public class AusschreibungMapper {
 		try {
 			Statement stmt = con.createStatement();
 			ResultSet rs = stmt.executeQuery(
-					"SELECT id, owner FROM ausschreibung " + "WHERE id=" + idAusschreibung + " ORDER BY ausschreibung");
+					"SELECT idAusschreibung, beschreibung, bezeichung, idProjekt, endDatum FROM ausschreibung " + "WHERE idAusschreibung=" + idAusschreibung + " ORDER BY ausschreibung");
 			if (rs.next()) {
 				Ausschreibung a = new Ausschreibung();
 				a.setIdAusschreibung(rs.getInt("idAusschreibung"));
@@ -51,7 +51,7 @@ public class AusschreibungMapper {
 		Vector<Ausschreibung> result = new Vector<Ausschreibung>();
 		try {
 			Statement stmt = con.createStatement();
-			ResultSet rs = stmt.executeQuery("SELECT idAusschreibung, owner FROM ausschreibung " + " ORDER BY idAusschreibung");
+			ResultSet rs = stmt.executeQuery("SELECT idAusschreibung, beschreibung, bezeichnung, idProjekt, endDatum FROM ausschreibung " + " ORDER BY idAusschreibung");
 			while (rs.next()) {
 				Ausschreibung a = new Ausschreibung();
 				a.setIdAusschreibung(rs.getInt("idAusschreibung"));
@@ -70,7 +70,7 @@ public class AusschreibungMapper {
 		try {
 			Statement stmt = con.createStatement();
 
-			ResultSet rs = stmt.executeQuery("SELECT idAusschreibung, owner FROM ausschreibung " + "WHERE owner=" + idAusschreibung + " ORDER BY idAusschreibung");
+			ResultSet rs = stmt.executeQuery("SELECT idAusschreibung, beschreibung, bezeichnung, idProjekt, endDatum FROM ausschreibung " + "WHERE owner=" + idAusschreibung + " ORDER BY idAusschreibung");
 			while (rs.next()) {
 				Ausschreibung a = new Ausschreibung();
 				a.setIdAusschreibung(rs.getInt("idAusschreibung"));
@@ -82,12 +82,7 @@ public class AusschreibungMapper {
 
 		return result;
 	}
-/*
-	public Vector<Ausschreibung> findByOwner(Organisationseinheit owner) {
-
-		return findByOwner(owner.getId());
-	}
-*/
+	
 	public Ausschreibung insert(Ausschreibung a) {
 		Connection con = DBConnection.connection();
 
@@ -120,9 +115,11 @@ public class AusschreibungMapper {
 		try {
 			Statement stmt = con.createStatement();
 			/// owner ?
-			stmt.executeUpdate("UPDATE ausschreibung" + "SET bezeichnug=\"" + a.getBezeichnung() + "\" ,"
-					+ "endDatum=\"" + a.getEndDatum() + "\"," + "beschreibung=\"" + a.getBeschreibung() + "WHERE id="
-					+ a.getIdAusschreibung());
+			stmt.executeUpdate("UPDATE ausschreibung" 
+					+ "SET bezeichnug=\"" + a.getBezeichnung() + "\" ,"
+					+ "endDatum=\"" + format.format(a.getEndDatum()) + "\"," 
+					+ "beschreibung=\"" + a.getBeschreibung() + "\","
+					+ "WHERE idAusschreibung=" + a.getIdAusschreibung());
 		} catch (SQLException e2) {
 			e2.printStackTrace();
 		}
@@ -135,7 +132,7 @@ public class AusschreibungMapper {
 		try {
 			Statement stmt = con.createStatement();
 
-			stmt.executeUpdate("DELETE FROM ausschreibung " + "WHERE id=" + a.getIdAusschreibung());
+			stmt.executeUpdate("DELETE FROM ausschreibung " + "WHERE idAusschreibung=" + a.getIdAusschreibung());
 
 		} catch (SQLException e2) {
 			e2.printStackTrace();
