df820bbe74e40ee25ed83124da2f3258141e2b5c
ThomasBurkhardt <tb112@hdm-stuttgart.de>
2017-05-26 14:49:56 +0200

    .Marktplatz.java - Korrektur toString() this.bezeichnung

---
+++ src/de/hdm/itprojekt/shared/bo/Marktplatz.java
@@ -0,0 +1,51 @@
+package de.hdm.itprojekt.shared.bo;
+
+public class Marktplatz {
+
+	private int idMarktplatz = 0;
+	private static final long serialVersionUID = 1L;
+	private String geschaeftsgebiet;
+	private String bezeichnung;
+	private int idProjekt;
+	
+	public String getBezeichnung() {
+		return bezeichnung;
+	}
+
+	public void setBezeichnung(String bezeichnung) {
+		this.bezeichnung = bezeichnung;
+	}
+	
+	public int getIdMarktplatz() {
+		return idMarktplatz;
+	}
+
+	public void setIdMarktplatz(int idMarktplatz) {
+		this.idMarktplatz = idMarktplatz;
+	}
+		
+	public String getGeschaeftsgebiet() {
+		return geschaeftsgebiet;
+	}
+
+	public void setGeschaeftsgebiet(String geschaeftsgebiet) {
+		this.geschaeftsgebiet = geschaeftsgebiet;
+	}
+
+	public int getIdProjekt() {
+		return idProjekt;
+	}
+
+	public void setIdProjekt(int idProjekt) {
+		this.idProjekt = idProjekt;
+	}
+
+	public String toString(){
+		return super.toString()+ " "
+		+ this.idMarktplatz +" "
+		+ this.geschaeftsgebiet + " "
+		+ this.bezeichnung; 
+		
+	}
+	
+}
