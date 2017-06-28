a6611bfb8ddfaa644997ed1a86ceed6cbd85451e
ClirimShahini <cs223@hdm-stuttgart.de>
2017-06-29 00:02:21 +0200

    .team und unternehmenseite bearbeitet.

--- src/de/hdm/itprojekt/client/gui/TeamSeite.java
+++ src/de/hdm/itprojekt/client/gui/TeamSeite.java
@@ -2,10 +2,13 @@ package de.hdm.itprojekt.client.gui;
 import java.util.Vector;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.cellview.client.CellTable;
 import com.google.gwt.user.cellview.client.TextColumn;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
@@ -13,6 +16,7 @@ import com.google.gwt.view.client.SelectionChangeEvent;
 import com.google.gwt.view.client.SelectionChangeEvent.Handler;
 import com.google.gwt.view.client.SingleSelectionModel;
 
+import de.hdm.itprojekt.client.Navigator;
 import de.hdm.itprojekt.client.Showcase;
 import de.hdm.itprojekt.shared.GreetingService;
 import de.hdm.itprojekt.shared.GreetingServiceAsync;
@@ -29,10 +33,16 @@ public class TeamSeite extends Showcase {
 	private VerticalPanel vpanelTeam = new VerticalPanel();
 	private Team t1 = new Team();
 	private Person p1 = new Person();
+	private RoleManagement rm = null;
+	private Navigator navi = null;
+	private Button teamBeitreten = new Button("Team beitreten");
+	private Button teamBearbeiten = new Button("Team bearbeiten");
+	private Button teamAnlegen = new Button("Team Anlegen");
 	final SingleSelectionModel<Team> ssmallteams = new SingleSelectionModel<Team>();
 	
-	public TeamSeite(final Person person){
-		this.p1 = person;
+	public TeamSeite(final RoleManagement rm, final Navigator navi){
+		this.rm=rm;
+		this.navi=navi;
 		
 		
 	}
@@ -49,42 +59,21 @@ public class TeamSeite extends Showcase {
 		teamtabelle.setWidth("100%", true);
 		teamtabelle.setStylePrimaryName("celltable");
 		vpanelTeam.add(teamtabelle);
+		hpanelTeam.add(teamAnlegen);
+		hpanelTeam.add(teamBearbeiten);
+		hpanelTeam.add(teamBeitreten);
 		
 		this.add(vpanelTeam);
-		
+		this.add(hpanelTeam);
 		teamtabelle.setSelectionModel(ssmallteams);
 
 		ssmallteams.addSelectionChangeHandler(new Handler() {
 			
 			@Override
 			public void onSelectionChange(SelectionChangeEvent event) {
-				// TODO Auto-generated method stub
-				t1 = ssmallteams.getSelectedObject();
-				ssmallteams.getSelectedObject().getId();
-				p1.setIdTeam(ssmallteams.getSelectedObject().getId());
-			
-					gwtproxy.savePerson(p1, new AsyncCallback<Void>() {
-
-						@Override
-						public void onFailure(Throwable caught) {
-							// TODO Auto-generated method stub
-							Window.alert("TEAM-ADD ging nicht");
-						}
-
-						@Override
-						public void onSuccess(Void result) {
-							
-							
-							// TODO Auto-generated method stub
-							Window.alert("team geändert");
-							Showcase showcase = new PersonSeite(p1);
-							RootPanel.get("Anzeige").clear();
-							RootPanel.get("Anzeige").add(showcase);
-						}
-					});
-				}
-			
-	});
+				
+			t1 = ssmallteams.getSelectedObject();
+			}});
 		TextColumn<Team> teamName = new TextColumn<Team>(){
 
 			@Override
@@ -96,6 +85,38 @@ public class TeamSeite extends Showcase {
 		
 	};
 
+	teamAnlegen.addClickHandler(new ClickHandler(){
+
+		@Override
+		public void onClick(ClickEvent event) {
+			// TODO Auto-generated method stub
+			t1 = ssmallteams.getSelectedObject();
+			ssmallteams.getSelectedObject().getId();
+			p1.setIdTeam(ssmallteams.getSelectedObject().getId());
+		
+				gwtproxy.savePerson(p1, new AsyncCallback<Void>() {
+
+					@Override
+					public void onFailure(Throwable caught) {
+						// TODO Auto-generated method stub
+						Window.alert("TEAM-ADD ging nicht");
+					}
+
+					@Override
+					public void onSuccess(Void result) {
+						
+						
+						// TODO Auto-generated method stub
+						Window.alert("team geändert");
+						Showcase showcase = new PersonSeite(p1);
+						RootPanel.get("Anzeige").clear();
+						RootPanel.get("Anzeige").add(showcase);
+					}
+				});
+			}
+			
+		});
+	
 		teamtabelle.addColumn(teamName, "Team-Name");
 		gwtproxy.getAllTeams(new getAlleTeamsausDB());
 }
