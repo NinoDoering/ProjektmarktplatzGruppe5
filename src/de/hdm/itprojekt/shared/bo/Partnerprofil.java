40113a23b465b96a6654fe0947f5c077c3d417d9
ThomasBurkhardt <tb112@hdm-stuttgart.de>
2017-07-01 17:22:16 +0200

    .BusinessObjects Partnerprofil, Person, Projekt, Team, Unternehmen - Kommentare hinzugefuegt

--- src/de/hdm/itprojekt/shared/bo/Partnerprofil.java
+++ src/de/hdm/itprojekt/shared/bo/Partnerprofil.java
@@ -4,29 +4,49 @@ public class Partnerprofil extends BusinessObject {
 	
 	private static final long serialVersionUID = 1L; //?
 	
+	/**
+	 * Null-Konstruktor zu Partnerprofil
+	 */
 	public Partnerprofil() {
 		// TODO Auto-generated constructor stub
 		
 	}
 
-
-
+	/**
+	 * Auslesen des Fremdschlüssels der Ausschreibung
+	 * @return getIdAusschreibung
+	 */
 	public int getIdAusschreibung(){
 		return getIdAusschreibung();
 	}
 	
+	/**
+	 * Auslesen des Fremdschlüssels der Bewerbung
+	 * @return getIdBewerbung
+	 */
 	public int getIdBewerbung(){
 		return getIdBewerbung();
 	}
 	
+	/**
+	 * Auslesen des Fremdschlüssels der Eigenschaft
+	 * @return getIdEigenschaft
+	 */
 	public int getIdEigenschaft(){
 		return getIdEigenschaft();
 	}
 	
+	/**
+	 * Auslesen des Fremdschlüssels der Organisationseinheit
+	 * @return getIdOrganisationseinheit
+	 */
 	public int getIdOrganisationseinheit(){
 		return getIdOrganisationseinheit();
 	}
 
+	/**
+	 * Erzeugen einer textuellen Darstellung eines Partnerprofils
+	 */
 	@Override
 	public String toString() {
 		return "Partnerprofil []";
